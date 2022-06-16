2752fd621dcf4006fd6a0e087688a4cb34a4a0f4
AL2002MI08 <alexandramizero@gmail.com>
2022-06-16 14:05:32 -0700

    capitalises all words of a string

---
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char str[] = "Expect the best\n";
+	char *ptr;
+
+	ptr = cap string(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
+}
